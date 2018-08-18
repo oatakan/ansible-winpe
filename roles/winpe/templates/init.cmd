d47f8faee69e670bdffe5f25ab71950cdb1ca598
oatakan <oatakan@gmail.com>
2018-08-18 16:07:35 -0500

    add more steps

--- roles/winpe/templates/init.cmd
+++ roles/winpe/templates/init.cmd
@@ -10,7 +10,9 @@ rem setup enviroment variables
 rem -------------------------------------------------------------------
 echo set COBBLER_SYSTEMNAME
 echo NEXT: %SYSTEMDRIVE%\opt\bootstrap\setsysname.cmd
+{% if enable_script_debug %}
 PAUSE
+{% endif %}
 call %SYSTEMDRIVE%\opt\bootstrap\setsysname.cmd
 
 
@@ -20,18 +22,30 @@ rem -------------------------------------------------------------------
 
 echo "get the remainder of the init scripts"
 echo NEXT: curl -s -o %TEMP%/mountmedia.cmd http://%COBBLER_SERV%:%COBBLER_PORT%/cblr/svc/op/script/system/%COBBLER_SYSNAME%/?script=mountmedia.cmd
+{% if enable_script_debug %}
 PAUSE
+{% endif %}
 curl -s -o %TEMP%/mountmedia.cmd http://%COBBLER_SERV%:%COBBLER_PORT%/cblr/svc/op/script/system/%COBBLER_SYSNAME%/?script=mountmedia.cmd
 
 
 echo NEXT: curl -s -o %TEMP%/getks.cmd http://%COBBLER_SERV%:%COBBLER_PORT%/cblr/svc/op/script/system/%COBBLER_SYSNAME%/?script=getks.cmd
+{% if enable_script_debug %}
 PAUSE
+{% endif %}
 curl -s -o %TEMP%/getks.cmd http://%COBBLER_SERV%:%COBBLER_PORT%/cblr/svc/op/script/system/%COBBLER_SYSNAME%/?script=getks.cmd
 
 
 echo NEXT: curl -s -o %TEMP%/runsetup.cmd http://%COBBLER_SERV%:%COBBLER_PORT%/cblr/svc/op/script/system/%COBBLER_SYSNAME%/?script=runsetup.cmd
+{% if enable_script_debug %}
 PAUSE
+{% endif %}
 curl -s -o %TEMP%/runsetup.cmd http://%COBBLER_SERV%:%COBBLER_PORT%/cblr/svc/op/script/system/%COBBLER_SYSNAME%/?script=runsetup.cmd
+
+echo NEXT: curl -s -o %TEMP%/runsetup.cmd http://%COBBLER_SERV%:%COBBLER_PORT%/cblr/svc/op/script/system/%COBBLER_SYSNAME%/?script=setupcomplete.cmd
+{% if enable_script_debug %}
+PAUSE
+{% endif %}
+curl -s -o %TEMP%/setupcomplete.cmd http://%COBBLER_SERV%:%COBBLER_PORT%/cblr/svc/op/script/system/%COBBLER_SYSNAME%/?script=setupcomplete.cmd
 
 
 rem -------------------------------------------------------------------
@@ -39,18 +53,36 @@ rem run setup
 rem -------------------------------------------------------------------
 
 echo NEXT:call %TEMP%\mountmedia.cmd
+{% if enable_script_debug %}
 PAUSE
+{% endif %}
 call %TEMP%\mountmedia.cmd
 
 echo NEXT:call %TEMP%\getks.cmd
+{% if enable_script_debug %}
 PAUSE
+{% endif %}
 call %TEMP%\getks.cmd
 
 echo NEXT:call %TEMP%\runsetup.cmd
+{% if enable_script_debug %}
 PAUSE
+{% endif %}
 call %TEMP%\runsetup.cmd
+{% if enable_script_debug %}
 PAUSE
+{% endif %}
 
+echo NEXT:call %TEMP%\setupcomplete.cmd
+{% if enable_script_debug %}
+PAUSE
+{% endif %}
+call %TEMP%\setupcomplete.cmd
+{% if enable_script_debug %}
+PAUSE
+{% endif %}
 
 echo FINISH to SETUP
+{% if enable_script_debug %}
 PAUSE
+{% endif %}
--- roles/winpe/templates/setsysname.cmd
+++ roles/winpe/templates/setsysname.cmd
@@ -2,12 +2,20 @@
 
 set COBBLER_MAC=
 
+{% if enable_script_debug %}
 PAUSE
+{% endif %}
 for /f "tokens=1 delims= "    %%H in ('nbtstat -n ^| find "UNIQUE"')                      do set COBBLER_HNAME=%%H
+{% if enable_script_debug %}
 PAUSE
+{% endif %}
 for /f "tokens=4 delims= "    %%M in ('nbtstat -a %COBBLER_HNAME% ^| find "MAC Address"') do set COBBLER_MAC=%%M
+{% if enable_script_debug %}
 PAUSE
+{% endif %}
 for /f "tokens=1-6 delims=- " %%a in ('echo %COBBLER_MAC%')                               do set COBBLER_MAC=%%a:%%b:%%c:%%d:%%e:%%f
 
+{% if enable_script_debug %}
 PAUSE
+{% endif %}
 for /f "delims= " %%S in ('curl -s -H "X-RHN-Provisioning-MAC-0: eth0 %COBBLER_MAC%" http://%COBBLER_SERV%:%COBBLER_PORT%/cblr/svc/op/autodetect') do set COBBLER_SYSNAME=%%S
